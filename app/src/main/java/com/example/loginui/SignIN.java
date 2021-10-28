3b68e3732987862a7e92a022f231edbd72c779a8
Eddie Gingco <gingcoeddie971@gmail.com>
2021-10-28 08:46:34 +0800

    First Commit

---
+++ app/src/main/java/com/example/loginui/SignIN.java
@@ -0,0 +1,16 @@
+package com.example.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SignIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+    }
+}
